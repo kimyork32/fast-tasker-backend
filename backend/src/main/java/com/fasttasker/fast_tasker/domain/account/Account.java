b2d462e1d4386fca10200a6fdce68d28808f7a69
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-10 02:34:44 -0500

    realization and documentation for this VO

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/account/Account.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/account/Account.java
@@ -1,31 +1,26 @@
 package com.fasttasker.fast_tasker.domain.account;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import java.util.UUID;
 
-/**
- * 
- */
+
+@Entity
+@Table(name = "account")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Account {
 
-    /**
-     * Default constructor
-     */
-    public Account() {
-    }
-
-    /**
-     * 
-     */
+    @Id
     private UUID taskerId;
 
-    /**
-     * 
-     */
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "email", unique = true, nullable = false))
     private Email email;
 
-    /**
-     * 
-     */
+
     private Password passwordHash;
 
     /**
