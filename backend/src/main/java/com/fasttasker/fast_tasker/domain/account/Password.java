8aee7cc3dab1b543114aa13cc44f2ded3ba1e053
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-10 02:35:15 -0500

    realization and documentation for this VO

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/account/Password.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/account/Password.java
@@ -1,37 +1,39 @@
 package com.fasttasker.fast_tasker.domain.account;
 
-import java.io.*;
-import java.util.*;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
- * 
+ * represent a VO for a password.
+ * this class is immutable and its main purpose is to encapsulate
+ * the security logic, ALWAYS storing a hash of the password,
+ * never the plaintext.
+ *
+ * @Embeddable is used so that JPA stores it as columns
+ * in the owner entity's table Account.
  */
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Password {
 
     /**
-     * Default constructor
-     */
-    public Password() {
-    }
-
-    /**
-     * 
+     * never stored the password in plain text.
      */
     private String value;
 
-
     /**
-     * 
+     *
+     * @param rawPassword password in plain text.
+     * @return true if the password matches. False otherwise.
      */
-    public Password() {
-        // TODO implement here
-    }
-
-    /**
-     * @param raw
-     */
-    public void verify(void raw) {
-        // TODO implement here
+    public boolean verify(String rawPassword) {
+        // TODO implement the hash verification logic
+        // Use BCryptPasswordEncoder
+        return false;
     }
 
 }
