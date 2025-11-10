33a50b1bb7ef18e8724c05c67dab2233dc9c1f1c
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-10 01:50:34 -0500

    implement VO Email how Embeddable

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/account/Email.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/account/Email.java
@@ -1,44 +1,15 @@
 package com.fasttasker.fast_tasker.domain.account;
 
-import java.io.*;
-import java.util.*;
+import jakarta.persistence.Embeddable;
+import lombok.*;
 
-/**
- * 
- */
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
 public class Email {
 
-    /**
-     * Default constructor
-     */
-    public Email() {
-    }
-
-    /**
-     * 
-     */
     private String value;
-
-
-    /**
-     * 
-     */
-    public Email() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void getValue() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void equal() {
-        // TODO implement here
-    }
 
 }
