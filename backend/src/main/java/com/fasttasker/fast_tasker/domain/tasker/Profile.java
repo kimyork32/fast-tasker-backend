8650249d67acc329432664b1b761c07054c310eb
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-10 22:17:29 -0500

    implement profile

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/Profile.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/Profile.java
@@ -1,49 +1,59 @@
 package com.fasttasker.fast_tasker.domain.tasker;
 
-import java.io.*;
-import java.util.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 
  */
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
 public class Profile {
-
-    /**
-     * Default constructor
-     */
-    public Profile() {
-    }
 
     /**
      * 
      */
+    @Column(name = "photo", length = 255, nullable = false)
     private String photo;
 
     /**
      * 
      */
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "latitude", column = @Column(name = "latitude", nullable = false)),
+            @AttributeOverride(name = "longitude", column = @Column(name = "longitude", nullable = false)),
+            @AttributeOverride(name = "address", column = @Column(name = "address", length = 255, nullable = false))
+    })
     private Location location;
 
     /**
      * 
      */
+    @Column(name = "about", length = 500, nullable = false)
     private String about;
 
     /**
      * 
      */
+    @Column(name = "reputation", nullable = false)
     private int reputation;
 
     /**
      * 
      */
+    @Column(name = "client_reviews", nullable = false)
     private int clientReviews;
 
     /**
      * 
      */
+    @Column(name = "completed_tasks", nullable = false)
     private int completedTasks;
-
-
-
 }
