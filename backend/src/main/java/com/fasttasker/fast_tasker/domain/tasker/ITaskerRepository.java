43788f6fa3b9b08fe35f47398b8db1fdcb9890f5
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-10 22:39:56 -0500

    implemented and fixing Tasker

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/ITaskerRepository.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/ITaskerRepository.java
@@ -1,14 +1,8 @@
 package com.fasttasker.fast_tasker.domain.tasker;
 
-import java.io.*;
-import java.util.*;
-
 /**
  * 
  */
 public interface ITaskerRepository {
-
-
-
 
 }
--- backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/Tasker.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/Tasker.java
@@ -1,43 +1,57 @@
 package com.fasttasker.fast_tasker.domain.tasker;
 
 
-import com.fasttasker.fast_tasker.domain.task.Task;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.util.List;
 import java.util.UUID;
 
 /**
  * 
  */
+@Entity
+@Table(name = "tasker")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Tasker {
-
-    /**
-     * Default constructor
-     */
-    public Tasker() {
-    }
 
     /**
      * 
      */
+    @Id
     private UUID id;
 
     /**
      * 
      */
+    @Column(name = "account_id", unique = true, nullable = false)
     private UUID accountId;
 
     /**
      * 
      */
+    @Embedded
+    @AttributeOverrides({
+            // Mapeo de campos directos de 'Profile'
+            @AttributeOverride(name = "photo", column = @Column(name = "profile_photo_url", length = 255)),
+            @AttributeOverride(name = "about", column = @Column(name = "profile_about", length = 500)),
+            @AttributeOverride(name = "reputation", column = @Column(name = "profile_reputation")),
+            @AttributeOverride(name = "clientReviews", column = @Column(name = "profile_client_reviews")),
+            @AttributeOverride(name = "completedTasks", column = @Column(name = "profile_completed_tasks")),
+
+            // Mapeo de campos anidados de 'Profile.location'
+            // Se usa la sintaxis "dot notation" (punto).
+            @AttributeOverride(name = "location.latitude", column = @Column(name = "location_latitude", nullable = false)),
+            @AttributeOverride(name = "location.longitude", column = @Column(name = "location_longitude", nullable = false)),
+            @AttributeOverride(name = "location.address", column = @Column(name = "location_address", length = 255, nullable = false))
+    })
     private Profile profile;
 
-    /**
-     * 
-     */
-    private List<Task> tasks;
-
-
+    // private List<Task> tasks; DELETED: i removed it because it breaks a fundamental rule of DDD called the Aggregates Rule
 
     /**
      * 
