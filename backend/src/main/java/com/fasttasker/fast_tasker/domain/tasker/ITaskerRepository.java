93ef38807dab6be2f031d22a6e1497d10591ea81
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-10 22:54:16 -0500

    implement Tasker Repository

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/ITaskerRepository.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/ITaskerRepository.java
@@ -1,8 +1,20 @@
 package com.fasttasker.fast_tasker.domain.tasker;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 /**
  * 
  */
 public interface ITaskerRepository {
+
+    Tasker save(Tasker tasker);
+
+    Optional<Tasker> findById(UUID id);
+
+    void deleteById(UUID id);
+
+    List<Tasker> findAll();
 
 }
--- backend/src/main/java/com/fasttasker/fast_tasker/persistence/JpaTaskerRepository.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/persistence/JpaTaskerRepository.java
@@ -1,16 +1,18 @@
 package com.fasttasker.fast_tasker.persistence;
 
 import com.fasttasker.fast_tasker.domain.tasker.ITaskerRepository;
+import com.fasttasker.fast_tasker.domain.tasker.Tasker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 /**
  * 
  */
-public class JpaTaskerRepository implements ITaskerRepository {
+@Repository
+public interface JpaTaskerRepository extends JpaRepository<Tasker, UUID>, ITaskerRepository {
 
-    /**
-     *
-    public TaskerRepository() {
-    }
-     */
+    // NOTHING
 
 }
