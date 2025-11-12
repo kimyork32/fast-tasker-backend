d144fa70813469ad7b3f381c5ec13951add8b02b
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-11 23:15:59 -0500

    add adapter for ITaskerRepository

---
+++ backend/src/main/java/com/fasttasker/fast_tasker/persistence/TaskerRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.fasttasker.fast_tasker.persistence;
+
+import com.fasttasker.fast_tasker.domain.tasker.ITaskerRepository;
+import com.fasttasker.fast_tasker.domain.tasker.Tasker;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class TaskerRepositoryImpl implements ITaskerRepository {
+
+    private final JpaTaskerRepository jpa;
+
+    public TaskerRepositoryImpl(JpaTaskerRepository jpa) {
+        this.jpa = jpa;
+    }
+
+    @Override
+    public Tasker save(Tasker tasker) {
+        return jpa.save(tasker);
+    }
+
+    @Override
+    public Optional<Tasker> findById(UUID id) {
+        return jpa.findById(id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        jpa.deleteById(id);
+    }
+
+    @Override
+    public List<Tasker> findAll() {
+        return jpa.findAll();
+    }
+
+    @Override
+    public Optional<Tasker> findByAccountId(UUID accountId) {
+        return jpa.findByAccountId(accountId);
+    }
+}
