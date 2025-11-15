6caee69667b3151f789ce550e867db82db1ceb00
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-15 06:25:21 -0500

    add Mapper for Task

---
+++ backend/src/main/java/com/fasttasker/fast_tasker/application/mapper/TaskMapper.java
@@ -0,0 +1,58 @@
+package com.fasttasker.fast_tasker.application.mapper;
+
+import com.fasttasker.fast_tasker.application.dto.task.TaskRequest;
+import com.fasttasker.fast_tasker.application.dto.task.TaskResponse;
+import com.fasttasker.fast_tasker.application.dto.tasker.LocationResponse;
+import com.fasttasker.fast_tasker.domain.task.Task;
+import com.fasttasker.fast_tasker.domain.task.TaskStatus;
+import com.fasttasker.fast_tasker.domain.tasker.Location;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Component
+public class TaskMapper {
+    public Task toEntity(TaskRequest request) {
+        if (request == null) return null;
+
+        var loc = request.location();
+
+        return new Task(
+                UUID.randomUUID(),
+                request.title(),
+                request.description(),
+                request.budget(),
+                new Location(loc.latitude(), loc.longitude(), loc.address()),
+                LocalDate.parse(request.taskDate()),
+                TaskStatus.valueOf(request.status()),
+                UUID.fromString(request.posterId()),
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+    }
+
+    public TaskResponse toResponse(Task task) {
+        if (task == null) return null;
+
+        var location = task.getLocation();
+
+        var locationResponse = LocationResponse.builder()
+                .latitude(location.getLatitude())
+                .longitude(location.getLongitude())
+                .address(location.getAddress())
+                .build();
+
+        return new TaskResponse(
+                task.getTitle(),
+                task.getDescription(),
+                task.getBudget(),
+                locationResponse,
+                task.getTaskDate().toString(),
+                task.getStatus().name(),
+                task.getPosterId().toString()
+        );
+    }
+
+}
