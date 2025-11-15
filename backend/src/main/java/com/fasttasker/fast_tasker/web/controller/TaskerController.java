ee7f0e59b30b91f1c2712fd62bf27b258c5b6468
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-15 05:08:45 -0500

    generate new token and add profileCompleted=true in this token

--- backend/src/main/java/com/fasttasker/fast_tasker/web/controller/TaskerController.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/web/controller/TaskerController.java
@@ -3,7 +3,10 @@ package com.fasttasker.fast_tasker.web.controller;
 import com.fasttasker.fast_tasker.application.TaskerService;
 import com.fasttasker.fast_tasker.application.dto.tasker.TaskerRequest;
 import com.fasttasker.fast_tasker.application.dto.tasker.TaskerResponse;
+import com.fasttasker.fast_tasker.config.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +21,16 @@ import java.util.UUID;
 public class TaskerController {
 
     private final TaskerService taskerService;
+    private final JwtService jwtService;
+    private final String AUTH_COOKIE_NAME = "jwtToken"; // this should be same as proxy.ts token
 
     /**
      * constructor for dependencies injection
      */
     @Autowired
-    public TaskerController(TaskerService taskerService) {
+    public TaskerController(TaskerService taskerService, JwtService jwtService) {
         this.taskerService = taskerService;
+        this.jwtService = jwtService;
     }
 
     @PutMapping("/register")
@@ -37,7 +43,20 @@ public class TaskerController {
         request = new TaskerRequest(accountId, request.profile());
 
         TaskerResponse response = taskerService.registerTasker(request);
-        return ResponseEntity.ok(response);
+
+        String newToken = jwtService.generateToken(accountId, true); // profileCompleted = true
+
+        ResponseCookie cookie = ResponseCookie.from(AUTH_COOKIE_NAME, newToken)
+                .httpOnly(false)
+                .secure(false) // enable this in production
+                .path("/")
+                .maxAge(60 * 60 * 24) // 1 day
+                .build();
+
+        // return profile and new token
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(response);
     }
 
     @GetMapping("/user/{userId}")
