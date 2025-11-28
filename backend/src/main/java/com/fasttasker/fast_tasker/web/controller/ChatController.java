21d385a2e643863573947aa65482546135163692
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-28 10:45:32 -0500

    implement: getInbox

--- backend/src/main/java/com/fasttasker/fast_tasker/web/controller/ChatController.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/web/controller/ChatController.java
@@ -1,12 +1,17 @@
 package com.fasttasker.fast_tasker.web.controller;
 
 import com.fasttasker.fast_tasker.application.ConversationService;
+import com.fasttasker.fast_tasker.application.dto.conversation.ConversationSummary;
 import com.fasttasker.fast_tasker.application.dto.conversation.MessageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.UUID;
 
 // reference: https://dev.to/supernovabirth/building-a-real-time-chatroom-with-spring-boot-and-websockets-3pk3
@@ -30,5 +35,12 @@ public class ChatController {
         // extract ID of the token
         UUID senderId = UUID.fromString(principal.getName());
         conversationService.processAndSendMessage(messageRequest, senderId);
+    }
+
+    @GetMapping("/api/v1/conversations/inbox")
+    public ResponseEntity<List<ConversationSummary>> getInbox(Authentication authentication) {
+        // extract ID of the token
+        UUID taskerId = UUID.fromString(authentication.getName());
+        return ResponseEntity.ok(conversationService.getUserInbox(taskerId));
     }
 }
