a18b7010ff42dd42e1fcc27c207340790a4f8173
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-28 10:09:20 -0500

    add chatController and implementation sendMessage

---
+++ backend/src/main/java/com/fasttasker/fast_tasker/web/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.fasttasker.fast_tasker.web.controller;
+
+import com.fasttasker.fast_tasker.application.ConversationService;
+import com.fasttasker.fast_tasker.application.dto.conversation.MessageRequest;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// reference: https://dev.to/supernovabirth/building-a-real-time-chatroom-with-spring-boot-and-websockets-3pk3
+// but this is to production, the "reference" is like helloWorld in webSockets
+@RestController
+public class ChatController {
+
+    private final ConversationService conversationService;
+
+    public ChatController(ConversationService conversationService) {
+        this.conversationService = conversationService;
+    }
+
+    /**
+     * receive message from the client
+     * part WEBSOCKETS (real time)
+     * @param messageRequest messageRequest
+     */
+    @MessageMapping("/chat/.send")
+    public void sendMessage(@Payload MessageRequest messageRequest) {
+        conversationService.processAndSendMessage(messageRequest);
+    }
+}
