33b6325744d04f25da33d19a4fda70e60c5d9c95
kimyork32 <CaronteGG.20851@gmail.com>
2025-11-28 06:23:23 -0500

    implement getUserInbox

--- backend/src/main/java/com/fasttasker/fast_tasker/application/ConversationService.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/application/ConversationService.java
@@ -1,13 +1,18 @@
 package com.fasttasker.fast_tasker.application;
 
 import com.fasttasker.fast_tasker.application.dto.conversation.ConversationRequest;
+import com.fasttasker.fast_tasker.application.dto.conversation.ConversationSummary;
 import com.fasttasker.fast_tasker.application.mapper.ConversationMapper;
 import com.fasttasker.fast_tasker.domain.conversation.Conversation;
 import com.fasttasker.fast_tasker.domain.conversation.IConversationRepository;
+import com.fasttasker.fast_tasker.domain.conversation.Message;
+import com.fasttasker.fast_tasker.domain.conversation.MessageContent;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ConversationService {
@@ -34,5 +39,35 @@ public class ConversationService {
                     Conversation newConversation = conversationMapper.toConversationEntity(conversationRequest);
                     return conversationRepository.save(newConversation).getId();
                 });
+    }
+
+    /**
+     * get user's inbox
+     * @param userId user id
+     * @return list of conversations
+     */
+    @Transactional(readOnly = true)
+    public List<ConversationSummary> getUserInbox(UUID userId) {
+        return conversationRepository.findByParticipantId(userId).stream()
+                .map(c -> {
+                    // calculate other id
+                    UUID otherId = c.getParticipantA().equals(userId) ? c.getParticipantB() : c.getParticipantA();
+                    // get last message
+                    MessageContent lastMessageContent = c.getMessages().getLast().getContent();
+                    String snippet = "";
+                    if (lastMessageContent.getText() != null) {
+                        snippet = lastMessageContent.getText();
+                    }
+                    else if (lastMessageContent.getAttachmentUrl() != null) {
+                        snippet = "Foto";
+                    }
+                    return new ConversationSummary(
+                            c.getId(),
+                            c.getTaskId(),
+                            otherId,
+                            snippet
+                    );
+                })
+                .collect(Collectors.toList());
     }
 }
