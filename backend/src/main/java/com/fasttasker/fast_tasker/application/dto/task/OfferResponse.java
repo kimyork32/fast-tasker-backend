792de042d45a7e2fcaeae33fe41db20bbad16435
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-02 12:22:34 -0500

    modifying offer response to send all attributes in string formant and int

--- backend/src/main/java/com/fasttasker/fast_tasker/application/dto/task/OfferResponse.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/application/dto/task/OfferResponse.java
@@ -1,16 +1,12 @@
 package com.fasttasker.fast_tasker.application.dto.task;
 
-import com.fasttasker.fast_tasker.domain.task.OfferStatus;
 import lombok.Builder;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @Builder
 public record OfferResponse(
-        UUID id,
+        String id,
         int price,
         String description,
-        OfferStatus status,
-        Instant createAt
+        String status,
+        String createAt
 ) {}
