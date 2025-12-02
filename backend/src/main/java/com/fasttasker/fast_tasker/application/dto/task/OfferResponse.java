c1f79a84816d9bccffdccd4b01214821d052070d
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-02 11:03:31 -0500

    add builder annotation for future uses

--- backend/src/main/java/com/fasttasker/fast_tasker/application/dto/task/OfferResponse.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/application/dto/task/OfferResponse.java
@@ -1,10 +1,12 @@
 package com.fasttasker.fast_tasker.application.dto.task;
 
 import com.fasttasker.fast_tasker.domain.task.OfferStatus;
+import lombok.Builder;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Builder
 public record OfferResponse(
         UUID id,
         int price,
