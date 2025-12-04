ea513c49d6a9caad6250ae40ca56450492aad9f0
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-04 07:54:01 -0500

    add Answer Entity in Task submodule

---
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/task/Answer.java
@@ -0,0 +1,39 @@
+package com.fasttasker.fast_tasker.domain.task;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+/**
+ * answer of the question
+ */
+@Entity
+@Table(name = "answer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Answer {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "description", length = 500, nullable = false)
+    private String description;
+
+    @Column(name = "question_id", nullable = false)
+    private UUID questionId;
+
+    @Column(name = "answered_id", nullable = false)
+    private UUID answeredId;
+
+    @Column(name = "created_at", nullable = false)
+    private String createdAt;
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    @ToString.Exclude
+    private Question question;
+}
