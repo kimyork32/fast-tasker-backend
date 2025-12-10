2bbdf9546c27b60af171a0b10cd5b3dbbd36548c
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-09 23:40:10 -0500

    add invariants in Location

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/Location.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/tasker/Location.java
@@ -8,30 +8,49 @@ import lombok.*;
  */
 @Embeddable
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @EqualsAndHashCode
 @ToString
 public class Location {
 
     /**
-     * 
+     * value between -90 and 90
      */
     private double latitude;
 
     /**
-     * 
+     * value between -180 and 180
      */
     private double longitude;
 
     /**
-     * 
+     * address of the location ...
      */
     private String address;
 
     /**
-     * zip code of peru
+     * zip code of peru (FOR THE MOMENT)
      */
     private int zip;
+
+    @Builder(toBuilder = true)
+    public Location(double latitude, double longitude, String address, int zip) {
+        if (latitude < -90 || latitude > 90) {
+            throw new IllegalArgumentException("latitude must be between -90 and 90");
+        }
+        if (longitude < -180 || longitude > 180) {
+            throw new IllegalArgumentException("longitude must be between -180 and 180");
+        }
+        if (zip < 0) {
+            throw new IllegalArgumentException("zip cannot be negative");
+        }
+
+        // validate address with zip here
+
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+        this.zip = zip;
+    }
 
 }
