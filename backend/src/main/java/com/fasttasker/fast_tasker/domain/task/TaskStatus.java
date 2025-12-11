3e267c1704c2f19fd302228224ace12ef2f6c3ef
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-11 18:38:15 -0500

    add documentation in TaskStatus

--- backend/src/main/java/com/fasttasker/fast_tasker/domain/task/TaskStatus.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/domain/task/TaskStatus.java
@@ -4,8 +4,33 @@ package com.fasttasker.fast_tasker.domain.task;
  * 
  */
 public enum TaskStatus {
+    /**
+     * the task is public and bids are being accepted.
+     * no one has been hired yet.
+     */
     ACTIVE,
+
+    /**
+     * the owner accepted an offer. The money is being held in escrow.
+     * the Tasker is committed to completing the job on the agreed-upon date.
+     * no new offers are being accepted.
+     */
+    ASSIGNED,
+
+    /**
+     * physical work has begun. Useful if you need to track time or location in real time.
+     * if you don't need that much precision, you can skip this and go from ASSIGNED to COMPLETED.
+     */
     IN_PROGRESS,
+
+    /**
+     * the job is complete and the payment has been released to the Tasker.
+     * cycle successfully completed.
+     */
     COMPLETED,
+
+    /**
+     * the task was canceled by the owner or there was a problem and it was canceled afterward
+     */
     CANCELLED
 }
