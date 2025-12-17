ba4832cf77a7a7fdc8b5cc3a4bed132c7e0bc4b4
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-17 18:20:35 -0500

    removing unused variable

--- backend/src/main/java/com/fasttasker/fast_tasker/config/DataSeeder.java
+++ backend/src/main/java/com/fasttasker/fast_tasker/config/DataSeeder.java
@@ -11,7 +11,6 @@ import com.fasttasker.fast_tasker.application.dto.task.TaskResponse;
 import com.fasttasker.fast_tasker.application.dto.tasker.LocationRequest;
 import com.fasttasker.fast_tasker.application.dto.tasker.ProfileRequest;
 import com.fasttasker.fast_tasker.application.dto.tasker.TaskerRequest;
-import com.fasttasker.fast_tasker.application.dto.tasker.TaskerResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -66,7 +65,7 @@ public class DataSeeder implements CommandLineRunner {
                 .build();
 
         taskerService.registerTasker(taskerRequest1);
-        TaskerResponse taskerResponse2 = taskerService.registerTasker(taskerRequest2);
+        taskerService.registerTasker(taskerRequest2);
 
 
         // simulating that the tasker creating a task
