4a542dcde0bb6465d3f77f969cda8229a1bcaffb
kimyork32 <CaronteGG.20851@gmail.com>
2025-12-17 19:43:02 -0500

    add sonar.yml workflow

--- backend/src/test/java/com/fasttasker/fast_tasker/application/TaskServiceTest.java
+++ backend/src/test/java/com/fasttasker/fast_tasker/application/TaskServiceTest.java
@@ -15,12 +15,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
