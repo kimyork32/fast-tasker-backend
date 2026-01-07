aa212f54261b49d24435031b9e10f0b580444f61
kimyork32 <CaronteGG.20851@gmail.com>
2026-01-07 07:25:20 -0500

    enlarged scan

--- monolith-app/src/main/java/com/fasttasker/fast_tasker/FastTaskerApplication.java
+++ monolith-app/src/main/java/com/fasttasker/fast_tasker/FastTaskerApplication.java
@@ -3,7 +3,10 @@ package com.fasttasker.fast_tasker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.fasttasker.fast_tasker",
+        "com.fasttasker.common"
+})
 public class FastTaskerApplication {
 
 	public static void main(String[] args) {
